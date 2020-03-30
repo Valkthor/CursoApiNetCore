b313c89d504c288f2d5e3e7574a14b5d09dfd84b
Valkthor <srojas@contaline.cl>
2020-03-29 21:43:20 -0300

    Se agrega proyecto para probar hostedService

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 * en las carpeta Helpers se dejan las validaciones personalizadas. 
 
 * la convencion de los atributos (de validaciones) tienen que estar terminadas por Atribute.
+
+
+## IHostedServiceDemo
+
+dentro de este repositorio tambien se encuentra un demo de la utilizacion del hostedService,
+metodo que nos permite ejecutar procesos cuando se levanta el o se apaga el servidor (corregire a medida que valla viendo el curso)
+
+es una web application MVC
+
---
+++ iHostedServiceDemo/Services/WriteToFileHostedService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace iHostedServiceDemo.Services
+{
+    //al traer las librerias de IhostedService, se crea automaticamente la implentacion de las task
+    public class WriteToFileHostedService : IHostedService
+    {
+        private readonly IHostingEnvironment environment;
+        private readonly string fileName = "File1.txt";
+
+        //se inyecta dependencias
+        public WriteToFileHostedService(IHostingEnvironment environment)
+        {
+            this.environment = environment;
+
+        }
+
+        private void WriteToFile(string message)
+        {
+            // environment.ContentRootPath indica en donde se encuentra mi aplicacion corriendo
+            // ojo con los espacios
+            var path = $@"{ environment.ContentRootPath }\wwwroot\{fileName }";
+            using (StreamWriter writer = new StreamWriter(path, append: true))
+            {
+                writer.WriteLine(message);
+            }
+        }
+
+        // implementacion de funcion recurrente, solo para que se entienda puse todo aca
+        private Timer timer;
+        
+        private void DoWork( object state)
+        {
+            WriteToFile("WriteToFileHostedService: haciendo algo en " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"));
+        }
+
+        //----------------------------------------------------------------
+        // las task creadas por defecto
+        Task IHostedService.StartAsync(CancellationToken cancellationToken)
+        {
+            // se escribe en el archivo cuando la aplicacion empiece.
+            WriteToFile("WriteToFileHostedService: Process Started");
+
+            //se inicializa el timer para la funcion dowork
+            timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+            //throw new NotImplementedException();
+            return Task.CompletedTask;
+        }
+
+        //segun la documentacion de microsoft, este evento es posible que no se ejecute
+        Task IHostedService.StopAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService: Process Stopped");
+            //throw new NotImplementedException();
+            return Task.CompletedTask;
+        }
+    }
+}
